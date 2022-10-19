9b75c988460718c31954428cb7d16d30d6c33000
Luis <lavp99@hotmail.com>
2022-10-19 21:23:18 +0200

    Finalizado con diferentes pruebas

--- src/app.js
+++ src/app.js
@@ -8,25 +8,24 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   // console.log("Hello Rigo from the console!");
-  let who = ['the dog','my granma','his turtle','my bird'];
+  // let who = ['the dog','my granma','his turtle','my bird'];
   let what = ['eat','pissed','crushed','broked'];
   let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
 
+  let  aleatorio = (max) => { 
+    let n = Math.round(Math.random() * (max-1));
+    console.log(n);
+    return n;
+  };
+  
   let who = () => {
     let arr = ['the dog','my granma','his turtle','my bird'];
-    return arr[Math.round(Math.random() * (4 - 1) + 1)] +' '+ 
-  }
+    let n = aleatorio(arr.length);
+    return arr[n];
+  };
 
   const valor = window.document.querySelector("#excuse");
-  valor.textContent = who[Math.round(Math.random() * (4 - 1) + 1)] +' '+ 
-                      what[Math.round(Math.random() * (4 - 1) + 1)] +' '+ 
-                      when[Math.round(Math.random() * (5 - 1) + 1)];
-
-/*
-valor.textContent = who[Math.round(Math.random() * (4 - 1) + 1)] +' '+ 
-                      what[Math.round(Math.random() * (4 - 1) + 1)] +' '+ 
-                      when[Math.round(Math.random() * (5 - 1) + 1)];
-
-*/
-
+  valor.textContent = who() +" "+ 
+                      what[aleatorio(what.length)] +' '+ 
+                      when[aleatorio(when.length)];
 };
